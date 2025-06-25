37b85dda61ebc0567c1080975cf79809079844da
Dominic Kipkorir <harsh979pal@gmail.com>
2025-06-25 21:29:05 +0300

    added backend url

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: ["http://localhost:5173", "https://skill-swap-project-k8cs.vercel.app"],
         credentials: true,
     })
 );
@@ -16,14 +16,14 @@ app.use(express.json({ limit: "16kb" })); // to parse json in body
 app.use(express.urlencoded({ extended: true, limit: "16kb" })); // to parse url
 app.use(express.static("public")); // to use static public folder
 app.use(cookieParser()); // to enable CRUD operation on browser cookies
-
+/*
 app.use(function (req, res, next) {
     res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
     res.setHeader("Access-Control-Allow-Credentials", "true");
     // Add other CORS headers as needed
     next();
 });
-
+*/
 // Passport middleware
 app.use(passport.initialize());
 
