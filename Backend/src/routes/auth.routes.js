df1f0d15669948906c165806a15fd3ead4b322bd
Dominic Kipkorir <harsh979pal@gmail.com>
2025-06-26 05:30:32 +0300

    debugging

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,34 +1,15 @@
-// src/routes/auth.routes.js
-
 import { Router } from "express";
-import passport from "passport";
 import {
+    googleAuthCallback,
+    googleAuthHandler,
     handleGoogleLoginCallback,
     handleLogout,
 } from "../controllers/auth.controllers.js";
 
 const router = Router();
 
-// Step 1: Start Google OAuth process
-router.get(
-    "/google",
-    passport.authenticate("google", {
-        scope: ["profile", "email"],
-        prompt: "select_account", // optional: to always choose account
-    })
-);
-
-// Step 2: Google redirects back here after user signs in
-router.get(
-    "/google/callback",
-    passport.authenticate("google", {
-        failureRedirect: "/login", // or your frontend login page
-        session: false, // depends on your setup
-    }),
-    handleGoogleLoginCallback
-);
-
-// Step 3: Logout
+router.get("/google", googleAuthHandler);
+router.get("/google/callback", googleAuthCallback, handleGoogleLoginCallback);
 router.get("/logout", handleLogout);
 
 export default router;
